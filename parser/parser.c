51c10a0634651c45275ff54fb5b2768b6e15b9ea
mac <mac@hostname.local>
2025-07-14 10:40:17 +0100

    almost done

--- parser/parser.c
+++ parser/parser.c
@@ -92,11 +92,49 @@ int	parse_arguments(t_data *data, t_elem **current, t_cmd *cmd)
 		if (!*current || (*current)->type == PIPE_LINE)
 			break;
 		// Handle both WORD and ENV tokens as arguments
-		if ((*current)->type == WORD || (*current)->type == ENV)
+		// 
+		else if ((*current)->type == WORD || (*current)->type == ENV)
+{
+	char *merged = ft_strdup((*current)->content);
+	if (!merged)
+		return (0);
+
+	t_elem *start = *current;
+	t_elem *next = (*current)->next;
+
+	while (next && (next->type == WORD || next->type == ENV))
+	{
+		// Check if there is whitespace between current and next
+		t_elem *tmp = start->next;
+		int separated_by_whitespace = 0;
+		while (tmp && tmp != next)
 		{
-			if (!process_word_token(data, current, cmd, &arg_index))
-				return (0);
+			if (tmp->type == WHITE_SPACE)
+			{
+				separated_by_whitespace = 1;
+				break;
+			}
+			tmp = tmp->next;
 		}
+		if (separated_by_whitespace)
+			break;
+
+		char *tmp_str = ft_strjoin(merged, next->content);
+		free(merged);
+		if (!tmp_str)
+			return (0);
+		merged = tmp_str;
+
+		start = next;
+		next = next->next;
+	}
+
+	if (cmd->full_cmd)
+		cmd->full_cmd[arg_index++] = merged;
+
+	*current = start->next;
+}
+
 		else if (!process_redirection(data, current, cmd))
 			return (0);
 	}
